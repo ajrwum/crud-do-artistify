ab504760b930b69e694555ca5b69b312648a6b34
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-10 10:56:18 +0100

    -board page & artist model unfinished

---
+++ models/artist.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const artistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+  },
+  isBand: Boolean,
+  description: String,
+  picture: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/gdaconcept/image/upload/v1614550771/workshop-artistify/no-image-logo_dcufai.png",
+  },
+});
